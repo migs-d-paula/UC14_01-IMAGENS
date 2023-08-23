9a603e1589fb151f49ace9f0624531cad4e3292a
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-08-22 22:10:03 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>imagens</title>
+</head>
+<body>
+    <div class="noticia">
+        <div class="noticia-imagem">
+            <img src="images/pequeno.png" alt="">
+        </div>
+        <div class="noticia-titulo">
+            <p> Turma A2022 de TI do Senac se forma em novembro de 2023 </p>
+        </div>
+        <div class="noticia-descricao">
+            <p> depois de passarem por diversos conteudos irão se formar finalmente </p>
+        </div>
+    </div>
+</body>
+</html>
